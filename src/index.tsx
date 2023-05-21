9d923a0f134eccd00f6956af4cd1f6019d6cc855
euxn23 <euxn23@gmail.com>
2023-05-21 20:44:14 +0900

    feat: adapt to twitter preview

--- src/index.tsx
+++ src/index.tsx
@@ -26,6 +26,19 @@ export default {
       return new Response('Bad parameter: title', { status: 400 })
     }
 
+    const isTwitterBot = request.headers.get('user-agent')?.includes('Twitterbot') ?? false;
+    if (isTwitterBot) {
+      const cache = await env.OGP_BLOG_EUXN_ME.get(`cache/${title}.png`);
+      if (cache) {
+        return new Response(cache.body, {
+          headers: {
+            'Cache-Status': '"Cloudflare R2"; hit',
+            'Content-Type': 'image/png'
+          }
+        })
+      }
+    }
+
     const cache = await fetch(`https://ogp.blog.euxn.me/cache/${title}.png`, { method: 'HEAD' });
     if (cache.status === 200) {
       return Response.redirect(`https://ogp.blog.euxn.me/cache/${title}.png`, 301)
